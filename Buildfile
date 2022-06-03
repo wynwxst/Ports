4efce9f190d86c4757c807813414c27bf3c897d8
Eris9 <cool5tarxv@gmail.com>
2022-06-03 12:50:19 +0800

    0.6.0

---
+++ Buildfile
@@ -0,0 +1,18 @@
+@function clean:
+rm -rf dist
+rm -rf ports.py.egg-info
+
+
+\endfunc
+
+@function build:
+@q python -m pip install --upgrade build
+@q python -m pip install --upgrade twine
+@q python -m build
+python3 -m twine upload --repository pypi dist/*
+\endfunc
+
+@function clean2:
+rm -rf dist
+rm -rf ports.py.egg-info
+\endfunc
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='ports.py',
+      version='0.6.0',
+      description='A modern webserver, check github for more information',
+      url='http://github.com/n30nyx/ports',
+      author='n30nyx/Sakurai07',
+      author_email='blzzardst0rm@gmail.com',
+      license='MIT',
+      packages=['ports'],
+    install_requires=[
+        'socket',"requests","sqlite3","websockets"
+        ],
+    keywords=['webserver', 'flask alternative','ports'],
+    classifiers=["Programming Language :: Python :: 3"],
+      zip_safe=False)
